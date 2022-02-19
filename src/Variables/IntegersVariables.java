e87e4a9055760b2e973472e59d1f114fc3f70731
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-19 20:29:44 +0300

    Variables Integers

---
+++ src/Variables/IntegersVariables.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class IntegersVariables {
+    public static void main(String[] args) {
+        byte vByte = 100; // 8 byte
+        short vShort = 1000; // 16 byte
+        int vInt = 2147400600; // 32 byte
+        long vLong = 1000000000;
+
+        System.out.println(vByte);
+        System.out.println(vShort);
+        System.out.println(vInt);
+        System.out.println(vLong);
+
+    }
+}
--- src/Variables/Variables.java
+++ src/Variables/Variables.java
@@ -2,6 +2,18 @@ package Variables;
 
 public class Variables {
     public static void main(String[] args) {
-        
+
+        final int numberOne = 1;
+        int numberTwo = 2, numberThree = 3;
+        System.out.println(numberOne);
+        System.out.println(numberTwo + numberThree + " kere selam");
+
+        int kisaKenar = 5;
+        int uzunKenar = 10;
+
+        int alan = kisaKenar * uzunKenar;
+        int cevre = 2 * (kisaKenar + uzunKenar);
+        System.out.println("Alan : "+alan + " Çevre : "+cevre);
+
     }
 }
