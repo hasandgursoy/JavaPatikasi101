e3103ae5b49d7be0c184e3b775b42cf6d34ad227
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-20 01:52:36 +0300

    Float Double

---
+++ src/Variables/FloatDouble.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class FloatDouble {
+
+    public static void main(String[] args) {
+
+        float vFloat = 3.14f; // 32 byte
+        double vDouble = 3.14; // 64 byte
+
+        // Double hassas matematik işlemleri için birebir çok güçlü bir yapıdır.
+
+        System.out.println(vFloat);
+        System.out.println(vDouble);
+
+
+
+    }
+
+}
