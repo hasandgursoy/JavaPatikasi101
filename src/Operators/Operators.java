202d2cae0807ab5e43c5e4a1319e5009e9cac704
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-20 15:10:33 +0300

    Operators and Input

---
+++ src/Operators/Operators.java
@@ -0,0 +1,25 @@
+package Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 5, b = 1;
+
+        System.out.println(a+b);
+
+
+        // Math Operators
+        // ++ , -- , /, * , %
+
+        // Comparison Operators
+        // == ,  !=, < , > , <= , >=
+
+        // Logical Operators
+        // && , || , !a , !b
+
+        // Condition Operators
+        // b = ( a == 1) ? 1 : 0
+
+
+    }
+}
