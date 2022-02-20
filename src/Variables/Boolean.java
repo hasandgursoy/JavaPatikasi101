1557c1fe55c408c4f643012e2a5d713f597dbcb4
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-20 14:58:00 +0300

    Boolean

---
+++ src/Variables/Boolean.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Boolean {
+
+    public static void main(String[] args) {
+        boolean dogru = true;
+        boolean yanlis = false;
+
+        System.out.println(dogru);
+        System.out.println(yanlis);
+
+
+
+    }
+
+
+
+}
