466b9131974ed48f0a5d4182fd3483543f0f885d
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-21 13:34:00 +0300

    Leap Year

---
+++ src/Exercises2/LeapYear.java
@@ -0,0 +1,21 @@
+package Exercises2;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int year;
+        System.out.print("Yıl Giriniz :");
+        year = scn.nextInt();
+
+        if ((year % 4 == 0)&& !(year %100 == 0)) {
+            System.out.println("Sayi Artıktır");
+        }else if (year % 400 == 0){
+            System.out.println("Sayi Artıktır.");
+        }else {
+            System.out.println("Sayi Artık Değildir.");
+        }
+
+    }
+}
