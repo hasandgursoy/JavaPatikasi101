93821534d9b4cc884a9449347f593b9f1c6fb12a
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-22 01:43:23 +0300

    Exponential Numbers

--- src/Exercises3/ExponentialNumbers.java
+++ src/Exercises3/ExponentialNumbers.java
@@ -1,8 +1,23 @@
 package Exercises3;
 
+import java.util.Scanner;
+
 public class ExponentialNumbers {
 
-    // Girilen sayıdan küçük 2'nin kuvvetlerini bulan program yazalım.
+    public static void main(String[] args) {
+
+        // Girilen sayıdan küçük 2'nin kuvvetlerini bulan program yazalım.
+        int inp;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Sayi Giriniz : ");
+        inp = scn.nextInt();
+        for (int i = 1; i <= inp; i=i*2){
+
+            System.out.println(i);
+        }
+
+    }
+
 
 
 }
