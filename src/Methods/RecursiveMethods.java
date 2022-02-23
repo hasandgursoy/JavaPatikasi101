efe93239f885006e35bab2b3bc5ac084c695d221
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-23 20:11:49 +0300

    Recursive Methods

---
+++ src/Methods/RecursiveMethods.java
@@ -0,0 +1,35 @@
+package Methods;
+
+public class RecursiveMethods {
+
+    static int r(int x){
+        int result = 0;
+        for (int i = 1; i<= x;i++){
+            result +=1;
+        }
+        return result;
+    }
+
+    static int f(int n){
+        System.out.println(n);
+        return f(n-1) +n;
+    }
+
+    public static void main(String[] args) {
+
+        // Recursive Method = Özyineli Method
+        // Bir methodun kendi kendini çağırma tekniğidir.
+        // Koşul sağlanmassa sonsuza kadar gider.
+        // f(1) = 1
+        // f(2) = 1 + 2
+        // f(3) = 1 + 2 + 3
+        // f(4) = 1 + 2 + 3 + 4
+        // f(n) = f(n-1) + n;
+
+        System.out.println(f(10));
+
+
+
+    }
+
+}
