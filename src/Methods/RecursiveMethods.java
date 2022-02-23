784c892af17d2824d65b75ec3c322b1fb23e6001
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-23 20:50:35 +0300

    Recursive arrangement

--- src/Methods/RecursiveMethods.java
+++ src/Methods/RecursiveMethods.java
@@ -12,6 +12,9 @@ public class RecursiveMethods {
 
     static int f(int n){
         System.out.println(n);
+        if (n == 1){
+            return 1;
+        }
         return f(n-1) +n;
     }
 
