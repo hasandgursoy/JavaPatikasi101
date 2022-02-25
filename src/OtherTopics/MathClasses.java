f152f9d4eb5054c5ec2c7db29b780c5e1e8a5eff
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-02-25 20:27:50 +0300

    Math Class

---
+++ src/OtherTopics/MathClasses.java
@@ -0,0 +1,55 @@
+package OtherTopics;
+
+public class MathClasses {
+
+    public static void main(String[] args) {
+
+        // 1. Mutlak Değer
+        System.out.println(Math.abs(-15));
+
+        // 2. Küp
+        System.out.println(Math.cbrt(9));
+
+        // 3. Yukarı Yuvarla
+        System.out.println(Math.ceil(1.2));
+
+        // 3. Aşşağı Yuvarla
+        System.out.println(Math.floor(1.9));
+
+        // 4. Max-Min
+        System.out.println(Math.max(100,200));
+        System.out.println(Math.min(100,200));
+
+        // 5. Üslü Sayı
+        System.out.println(Math.pow(2,5));
+
+        // 6. Random
+        System.out.println(Math.random()*100);
+
+        // 7 . İnt'e Yuvarla
+        System.out.println(Math.round(3.3));
+
+       // 8. Double'a Yuvarla
+        System.out.println(Math.rint(3));
+
+        // 9. İşaret Göster (-,+)
+        System.out.println(Math.signum(-1000));
+
+        // 10. Kare Kök
+        System.out.println(Math.sqrt(16));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
